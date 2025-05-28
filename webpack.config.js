9c48028b1c2317f2bd15aa7d7c645d53cf4e119e
hdwhite1980 <150296028+hdwhite1980@users.noreply.github.com>
2025-05-28 14:55:10 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
     filename: 'casl-bundle.js',
     path: path.resolve(__dirname, 'build/js'),
